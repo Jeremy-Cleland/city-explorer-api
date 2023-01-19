a45f066904e228431d9428b471d59c94fb6f58ea
Jeremy Cleland <jeremy@clelandco.com>
2023-01-18 20:08:37 -0500

    lab8

--- server.js
+++ server.js
@@ -6,55 +6,54 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 
-let data = require("./data/weather.json");
-
+// **** APP DECLARATION ****
 const app = express();
 
 app.use(cors());
+
 const PORT = process.env.PORT || 3002;
 
 app.get("/", (request, response) => {
   response.status(200).send("Welcome to my server");
 });
 
-// weather?lat=value&lon=value&searchQuery=value
-// *** DEFINE WEATHER ENDPOINT WITH THE FOLLOWING QUERIES - lat, lon, searchQuery
-app.get("/weather", (request, response, next) => {
+// **** ROUTES ****
+app.get("/weather", async (request, response, next) => {
   try {
     let lat = request.query.lat;
     let lon = request.query.lon;
-    let cityName = request.query.searchQuery;
 
-    console.log(request.query);
+    let url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}&days=5&units=I`;
 
-    let city = data.find(
-      (city) => city.city_name.toLowerCase() === cityName.toLowerCase()
-    );
+    let forcastResults = await axios.get(url);
 
-    let weatherData = city.data.map((dayObj) => new Forecast(dayObj));
-
+    let weatherData = forcastResults.data.data.map((day) => {
+      return new Forecast(day);
+    });
     response.status(200).send(weatherData);
   } catch (error) {
     next(error);
   }
 });
+
 // **** FORECAST CLASS TO GROOM BULKY DATA ****
 class Forecast {
-  constructor(dayObj) {
-    this.date = dayObj.valid_date;
-    this.description = dayObj.weather.description;
+  constructor(day) {
+    this.date = day.valid_date;
+    this.description = `Low of ${day.low_temp}, high of ${day.high_temp} with ${day.weather.description}`;
   }
 }
 
-// **** CATCH ALL ENDPOINT - NEEDS TO BE YOUR LAST DEFINED ENDPOINT ****
-app.get("*", (request, response) => {
-  response.status(404).send("This page does not exist");
+// **** ERROR HANDLERS ****
+app.use("*", (request, response) => {
+  response.status(404).send("Sorry, that route does not exist");
 });
 
-// **** ERROR HANDLING - PLUG AND PLAY CODE FROM EXPRESS DOCS ****
 app.use((error, request, response, next) => {
-  response.status(500).send(error.message);
+  response.status(500).send(error);
 });
 
-// ***** SERVER START ******
-app.listen(PORT, () => console.log(`We are running on port: ${PORT}`));
+// *** START SERVER ***
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
